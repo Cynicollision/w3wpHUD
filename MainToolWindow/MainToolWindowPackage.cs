332c3212bf3e21df138d44891e5436ff0448f833
Sean Normoyle <stn825@gmail.com>
2021-07-17 13:56:05 -0400

    use AsyncPackage

--- MainToolWindow/MainToolWindowPackage.cs
+++ MainToolWindow/MainToolWindowPackage.cs
@@ -4,22 +4,16 @@ using System.Runtime.InteropServices;
 
 namespace w3wpHUD
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(MainToolWindow))]
     [Guid(PackageGuidString)]
-    public sealed class MainToolWindowPackage : Package
+    public sealed class MainToolWindowPackage : AsyncPackage
     {
         /// <summary>
         /// MainToolWindowPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "afd931cf-9cf0-412d-8bb2-256af004f99f";
-
-        protected override void Initialize()
-        {
-            MainToolWindowCommand.Initialize(this);
-            base.Initialize();
-        }
     }
 }
